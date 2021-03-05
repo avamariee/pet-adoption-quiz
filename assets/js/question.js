f87502e25b8693159d471845b42c47e7f01250d7
Tess Austin <tessaustin92@gmail.com>
2021-03-05 11:28:19 -0700

    questions layed out

---
+++ assets/js/question.js
@@ -0,0 +1,25 @@
+//list of questions and choices
+var questions = [
+
+    {
+        title: "On a Friday night would you rather stay in or go to the club?",
+        choices: ["Defenitely stay in!", "Hello..? PARTY!"]
+    },
+    {
+        title: "Which would you prefer to eat for dinner?",
+        choices: ["Sushi", "T-Bone Steak"]
+    },
+    {
+        title: "Would you consider yourself independent or dependent?",
+        choices: ["Independent", "Dependent"]
+    },
+    {
+        title: "How often do you shower?",
+        choices: ["One-two times a week", "Three-seven times a week"]
+    },
+    {
+        title: "What is your honest opinion about vacuum cleaners?",
+        choices: ["They are terrifying, RUN AWAY!", "Must destroy!!!"]
+    }
+
+];
