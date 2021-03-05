871917ecb78fc0c1e03821334f798005c0a7ebba
Tess Austin <tessaustin92@gmail.com>
2021-03-05 12:39:32 -0700

    choices pop up after question title

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,13 +7,29 @@ var currentQuestionI = 0
 
 
 function start() {
+    //get questions
     theQuestions(); 
 }
 
 
 function theQuestions() {
+    //current object from question array
     var current = questions[currentQuestionI];
+
+    //current title 
     titleE1.textContent = current.title;
+
+    current.choices.forEach(function(choice, i ) {
+        var choicePick = document.createElement("li");
+        choicePick.setAttribute("class", "choice");
+        choicePick.setAttribute("value", choice);
+
+        choicePick.textContent = i + 1 + ". " + choice;
+
+        choicesEl.appendChild(choicePick);
+    })
+
 }
 
+//start quiz
 startbtn.onclick = start;
