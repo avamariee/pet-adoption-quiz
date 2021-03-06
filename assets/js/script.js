c9322162f88bc9ea0d0ed2098ffbf0759681bd0c
Ava Bullock <ava.m.bullock@gmail.com>
2021-03-05 22:10:48 -0700

    added APIs for cats and dogs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
-// stuff
-fetch();
+// fetch request code for the API's
+
+
+// random cat image API
+var kittykey = "UEjr9nH6HP9ZmWwr0s7mxku-9mpS7dUGmkilXBrhRcM"
+
+function kittyFetch() {
+
+
+    fetch(
+        // api
+        'https://api.unsplash.com/photos/?client_id='
+        +
+        kittykey
+    )
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+
+        })
+
+}
+
+kittyFetch();
+
+// random dog image API
+
+function dogFetch() {
+
+    fetch(
+        'https://dog.ceo/api/breeds/image/random'
+    )
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+dogFetch();
